833cfc431588938f34a0f2e96cb7760ed3c297ab
virtualglitch2009 <virtualglitch2009@gmail.com>
2025-02-06 19:59:45 +0000

    Firebase project initialized, credentials added to main.dart and pubspec.yaml files

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,22 @@
 import 'package:flutter/material.dart';
 //testing
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FireBaseoptions(
+            apiKey: "AIzaSyA3-e9JoKGpO2kzVXEPT84qWMF5OmOCLCE",
+            authDomain: "my-mindmend-project.firebaseapp.com",
+            projectId: "my-mindmend-project",
+            storageBucket: "my-mindmend-project.firebasestorage.app",
+            messagingSenderId: "233741423088",
+            appId: "1:233741423088:web:a4cb1a113f6c320c99cd35"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(MyApp());
 }
 
@@ -78,4 +93,3 @@ class _UserCredentialsInputScreenState
     );
   }
 }
-
